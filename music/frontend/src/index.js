7cb23b678986e6966a19b9d58762549daf9450f8
jaehwip <jaehwi.p@wustl.edu>
2021-03-22 17:14:54 -0500

    piano keyboard

--- music/frontend/src/index.js
+++ music/frontend/src/index.js
@@ -1,17 +1,84 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+import 'react-piano/dist/styles.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import DimensionsProvider from './DimensionsProvider';
+import SoundfontProvider from './SoundfontProvider';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// webkitAudioContext fallback needed to support Safari
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+const noteRange = {
+  first: MidiNumbers.fromNote('c3'),
+  last: MidiNumbers.fromNote('f4'),
+};
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: noteRange.first,
+  lastNote: noteRange.last,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW,
+});
+
+function App() {
+  return (
+    <div>
+
+      <div className="mt-5">
+        <BasicPiano />
+      </div>
+
+      <div className="mt-5">
+        <ResponsivePiano />
+      </div>
+
+    </div>
+  );
+}
+
+function BasicPiano() {
+  return (
+    <SoundfontProvider
+      instrumentName="acoustic_grand_piano"
+      audioContext={audioContext}
+      hostname={soundfontHostname}
+      render={({ isLoading, playNote, stopNote }) => (
+        <Piano
+          noteRange={noteRange}
+          width={300}
+          playNote={playNote}
+          stopNote={stopNote}
+          disabled={isLoading}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      )}
+    />
+  );
+}
+
+function ResponsivePiano(props) {
+  return (
+    <DimensionsProvider>
+      {({ containerWidth, containerHeight }) => (
+        <SoundfontProvider
+          instrumentName="acoustic_grand_piano"
+          audioContext={audioContext}
+          hostname={soundfontHostname}
+          render={({ isLoading, playNote, stopNote }) => (
+            <Piano
+              noteRange={noteRange}
+              width={containerWidth}
+              playNote={playNote}
+              stopNote={stopNote}
+              disabled={isLoading}
+              {...props}
+            />
+          )}
+        />
+      )}
+    </DimensionsProvider>
+  );
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
