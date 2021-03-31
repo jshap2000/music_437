b1f4576fbb10c19179e161e191d99909d5e600fc
Daniel <danielruffing@wustl.edu>
2021-03-30 19:25:04 -0500

    create MidiAnalyzer to convert midis to json

---
+++ music/backend/midi/midi_analyzer/JsonNote.py
@@ -0,0 +1,5 @@
+class JsonNote:
+    def __init__(self,midiNumber,time):
+        self.midiNumber = midiNumber
+        self.time = time
+        return
---
+++ music/backend/midi/midi_analyzer/MidiAnalyzer.py
@@ -0,0 +1,15 @@
+from MidiData import MidiData
+from JsonNote import 
+
+# Takes in a midi file and returns a python list containing each note with 'midiNumber' and 'time'
+# call json.dumps(jsonNoteList) to convert to json format
+
+def analyzeMidiFile(midi_file):
+    midiData = MidiData(midi_file)
+    for i in range(midiData.getNumTracks()):
+        track = midiData.getTrack(i)
+        jsonNoteList = []
+        for note in track.notes:
+            jsonNote = jsonNote(note.pitch, str("{0:.2f}".format(round(note.startTime*.001, 2))))
+            jsonNoteList.append(jsonNote.__dict__)
+    return jsonNoteList
