9df1acc2ac7cb21d45a14e7cc9db0dba80ab82d3
Josh Shapiro <joshshapiro@Joshs-MacBook-Pro.local>
2021-04-16 12:18:35 -0400

    a few fixes

--- music/frontend/src/index.js
+++ music/frontend/src/index.js
@@ -32,9 +32,6 @@ const {
 } = Vex.Flow;
 
 
-
-
-
 // webkitAudioContext fallback needed to support Safari
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
@@ -68,16 +65,9 @@ class App extends React.Component {
 
   constructor(props) {
     super(props);
-
     this.scheduledEvents = [];
-    this.scoreElem = React.createRef();
-    
   }
   
-
-  //
-  // React-Piano
-  //
   getRecordingEndTime = () => {
     if (this.state.recording.events.length === 0) {
       return 0;
@@ -116,18 +106,10 @@ class App extends React.Component {
     });
   };
    
-  //
-  // API
-  // 
 
-
-    componentWillMount() {
-      this.getOptions()
-    }
-
-
-
-    
+  componentWillMount() {
+    this.getOptions()
+  }
 
   async getOptions() {
     const res = await axios.get('/playable_pieces');
@@ -144,19 +126,17 @@ class App extends React.Component {
   handleOptionsChange(val){
     this.setState({title: val.value, notes: val.notes, grading: val.grading}, () => {
       document.addEventListener('keypress', (e) => {
-        console.log("keypress");
-        console.log(this.state.playing);
         if(this.state.playing==false) {
+          this.onClickClear();
           this.afterSetStateFinished();
         }
       });
-  });
-}
+    });
+  }
 
 afterSetStateFinished() {
     this.state.playing=true;
-    console.log("back here");
-    var div = document.getElementById("boo")
+    var div = document.getElementById("music")
     var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
       
       // var renderer = new Renderer(svgContainer, Renderer.Backends.SVG);
@@ -183,82 +163,106 @@ afterSetStateFinished() {
       // notes, and space them relative to each other based on their duration &
       // the space available. We definitely do not want that here! So, instead
       // of creating a voice, we handle that part of the drawing manually.
-      var tickContext = new VF.TickContext();
+    var tickContext = new VF.TickContext();
 
-      // Create a stave of width 10000 at position 10, 40 on the canvas.
-      var stave = new VF.Stave(10, 10, 10000)
-      .addClef('treble');
+    // Create a stave of width 10000 at position 10, 40 on the canvas.
+    var stave = new VF.Stave(10, 10, 10000)
+    .addClef('treble');
 
-      var stave2 = new VF.Stave(10, 100, 10000)
-      .addClef('bass');
+    var stave2 = new VF.Stave(10, 100, 10000)
+    .addClef('bass');
 
-      // Connect it to the rendering context and draw!
-      stave.setContext(context).draw();
-      stave2.setContext(context).draw();
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
+    stave2.setContext(context).draw();
 
-      var durations = [];
-      var notes_dict = JSON.parse((String(this.state.notes).replace(/'/g,'"').replace(/\.0/g,".0").replace(/\.5/g,".5")));
-     // ['c', '', 2]
-      var end_time = 0
-      var keys = []
-      let notes_treble = [];
-      let notes_bass = [];
-      for (var key in notes_dict) {
-        let note1 = notes_dict[key];
+    var durations = [];
+    var notes_dict = JSON.parse((String(this.state.notes).replace(/'/g,'"').replace(/\.0/g,".0").replace(/\.5/g,".5")));
+    // ['c', '', 2]
+    var end_time = 0
+    var keys = []
+    let notes_treble = [];
+    let notes_bass = [];
+    for (var key in notes_dict) {
+      let note1 = notes_dict[key];
+      for (var n of note1) {
+        if(n[0]<=58) {
+          var appendNote = [n[1].toLowerCase(), n[3].toLowerCase().trim(), n[2]];
+          notes_bass.push(appendNote);
+        } else {
+          var appendNote = [n[1].toLowerCase(), n[3].toLowerCase().trim(), n[2]];
+          notes_treble.push(appendNote);
+        }
+      }
+      end_time = parseFloat(key);
+    }
+
+    var bass_arr = [];
+    var treble_arr= [];
+    for(var a = 0; a < end_time; a+=0.25) {
+        var time_str = String(a);
+        if(a%1==0) {
+          time_str+=".0"
+        } 
+        
+      if(notes_dict[time_str]) {
+        let treb_counter = 0;
+        let bass_counter = 0;
+        let note1 = notes_dict[time_str];
         for (var n of note1) {
           if(n[0]<=58) {
-            var appendNote = [n[1].toLowerCase(), n[3].toLowerCase().trim(), n[2]];
-            notes_bass.push(appendNote);
+            bass_counter+=1;
           } else {
-            var appendNote = [n[1].toLowerCase(), n[3].toLowerCase().trim(), n[2]];
-            notes_treble.push(appendNote);
+            treb_counter+=1;
           }
         }
-        end_time = parseFloat(key);
+
+        bass_arr.push(bass_counter);
+        treble_arr.push(treb_counter);
+
+
+      } else {
+        bass_arr.push(0);
+        treble_arr.push(0);
+
       }
 
-      var bass_arr = [];
-      var treble_arr= [];
-      for(var a = 0; a < end_time; a+=0.25) {
-         var time_str = String(a);
-         if(a%1==0) {
-           time_str+=".0"
-         } 
-         
-        if(notes_dict[time_str]) {
-          let treb_counter = 0;
-          let bass_counter = 0;
-          let note1 = notes_dict[time_str];
-          for (var n of note1) {
-            if(n[0]<=58) {
-              bass_counter+=1;
-            } else {
-              treb_counter+=1;
-            }
-          }
-
-          bass_arr.push(bass_counter);
-          treble_arr.push(treb_counter);
-
-
-        } else {
-          bass_arr.push(0);
-          treble_arr.push(0);
-
-        }
-
-        
-      }
       
-      
-      let notes = notes_treble.map(([letter, acc, octave]) => {
+    }
+    
+    
+    let notes = notes_treble.map(([letter, acc, octave]) => {
+    const note = new VF.StaveNote({
+        clef: 'treble',
+        keys: [`${letter}${acc}/${octave}`],
+        duration: '4',
+        })
+        .setContext(context)
+        .setStave(stave);
+
+    // If a StaveNote has an accidental, we must render it manually.
+    // This is so that you get full control over whether to render
+    // an accidental depending on the musical context. Here, if we
+    // have one, we want to render it. (Theoretically, we might
+    // add logic to render a natural sign if we had the same letter
+    // name previously with an accidental. Or, perhaps every twelfth
+    // note or so we might render a natural sign randomly, just to be
+    // sure our user who's learning to read accidentals learns
+    // what the natural symbol means.)
+    if (acc) note.addAccidental(0, new VF.Accidental(acc));
+    tickContext.addTickable(note)
+    return note;
+    });
+
+
+    let notes3 = notes_bass.map(([letter, acc, octave]) => {
       const note = new VF.StaveNote({
-          clef: 'treble',
+          clef: 'bass',
           keys: [`${letter}${acc}/${octave}`],
           duration: '4',
           })
           .setContext(context)
-          .setStave(stave);
+          .setStave(stave2);
 
       // If a StaveNote has an accidental, we must render it manually.
       // This is so that you get full control over whether to render
@@ -270,98 +274,38 @@ afterSetStateFinished() {
       // sure our user who's learning to read accidentals learns
       // what the natural symbol means.)
       if (acc) note.addAccidental(0, new VF.Accidental(acc));
-      tickContext.addTickable(note)
+      tickContext.addTickable(note);
       return note;
-      });
+    });
 
 
-      let notes3 = notes_bass.map(([letter, acc, octave]) => {
-        const note = new VF.StaveNote({
-            clef: 'bass',
-            keys: [`${letter}${acc}/${octave}`],
-            duration: '4',
-            })
-            .setContext(context)
-            .setStave(stave2);
-  
-        // If a StaveNote has an accidental, we must render it manually.
-        // This is so that you get full control over whether to render
-        // an accidental depending on the musical context. Here, if we
-        // have one, we want to render it. (Theoretically, we might
-        // add logic to render a natural sign if we had the same letter
-        // name previously with an accidental. Or, perhaps every twelfth
-        // note or so we might render a natural sign randomly, just to be
-        // sure our user who's learning to read accidentals learns
-        // what the natural symbol means.)
-        if (acc) note.addAccidental(0, new VF.Accidental(acc));
-        tickContext.addTickable(note)
-        return note;
-        });
-
-
-      // The tickContext.preFormat() call assigns x-values (and other
-      // formatting values) to notes. It must be called after we've 
-      // created the notes and added them to the tickContext. Or, it
-      // can be called each time a note is added, if the number of 
-      // notes needed is not known at the time of bootstrapping.
-      //
-      // To see what happens if you put it in the wrong place, try moving
-      // this line up to where the TickContext is initialized, and check
-      // out the error message you get.
-      //
-      // tickContext.setX() establishes the left-most x position for all
-      // of the 'tickables' (notes, etc...) in a context.
-      tickContext.preFormat().setX(400);
-      //context.preFormat().setX(400);
-      // This will contain any notes that are currently visible on the staff,
-      // before they've either been answered correctly, or plumetted off
-      // the staff when a user fails to answer them correctly in time.
-      // TODO: Add sound effects.
-      const visibleNoteGroups = [];
-      // Add a note to the staff from the notes array (if there are any left).
-      var counter = 0;
+    // The tickContext.preFormat() call assigns x-values (and other
+    // formatting values) to notes. It must be called after we've 
+    // created the notes and added them to the tickContext. Or, it
+    // can be called each time a note is added, if the number of 
+    // notes needed is not known at the time of bootstrapping.
+    //
+    // To see what happens if you put it in the wrong place, try moving
+    // this line up to where the TickContext is initialized, and check
+    // out the error message you get.
+    //
+    // tickContext.setX() establishes the left-most x position for all
+    // of the 'tickables' (notes, etc...) in a context.
+    tickContext.preFormat().setX(400);
+    //context.preFormat().setX(400);
+    // This will contain any notes that are currently visible on the staff,
+    // before they've either been answered correctly, or plumetted off
+    // the staff when a user fails to answer them correctly in time.
+    // TODO: Add sound effects.
+    const visibleNoteGroups = [];
+    // Add a note to the staff from the notes array (if there are any left).
+    var counter = 0;
+    
+    this.state.interval = setInterval(() => {
       
-      this.state.interval = setInterval(() => {
-        
-        
-        for(var i = 0; i < treble_arr[counter]; i++) {
-            var note = notes.shift();
-            if (!note) return;
-            
-            const group = context.openGroup();
-            visibleNoteGroups.push(group);
-            note.draw();
-        
-        
-        
-        
-        context.closeGroup();
-        group.classList.add('scroll');
-        // Force a dom-refresh by asking for the group's bounding box. Why? Most
-        // modern browsers are smart enough to realize that adding .scroll class
-        // hasn't changed anything about the rendering, so they wait to apply it
-        // at the next dom refresh, when they can apply any other changes at the
-        // same time for optimization. However, if we allow that to happen,
-        // then sometimes the note will immediately jump to its fully transformed
-        // position -- because the transform will be applied before the class with
-        // its transition rule. 
-        const box = group.getBoundingClientRect();
-        group.classList.add('scrolling');
-
-        // If a user doesn't answer in time make the note fall below the staff
-        window.setTimeout(() => {
-            //const index = visibleNoteGroups.indexOf(group);
-           // if (index === -1) return;
-           group.classList.add('too-slow');
-           // visibleNoteGroups.shift();
-        }, 5000); }
-
-
-
-
-
-        for(var i = 0; i < bass_arr[counter]; i++) {
-          var note = notes3.shift();
+      
+      for(var i = 0; i < treble_arr[counter]; i++) {
+          var note = notes.shift();
           if (!note) return;
           
           const group = context.openGroup();
@@ -387,18 +331,54 @@ afterSetStateFinished() {
       // If a user doesn't answer in time make the note fall below the staff
       window.setTimeout(() => {
           //const index = visibleNoteGroups.indexOf(group);
-         // if (index === -1) return;
-         group.classList.add('too-slow');
-         // visibleNoteGroups.shift();
+          // if (index === -1) return;
+          group.classList.add('too-slow');
+          // visibleNoteGroups.shift();
       }, 5000); }
 
-      counter+=1;
-
-      
 
 
 
-        }, 250);
+
+      for(var i = 0; i < bass_arr[counter]; i++) {
+        var note = notes3.shift();
+        if (!note) return;
+        
+        const group = context.openGroup();
+        visibleNoteGroups.push(group);
+        note.draw();
+    
+    
+    
+    
+    context.closeGroup();
+    group.classList.add('scroll');
+    // Force a dom-refresh by asking for the group's bounding box. Why? Most
+    // modern browsers are smart enough to realize that adding .scroll class
+    // hasn't changed anything about the rendering, so they wait to apply it
+    // at the next dom refresh, when they can apply any other changes at the
+    // same time for optimization. However, if we allow that to happen,
+    // then sometimes the note will immediately jump to its fully transformed
+    // position -- because the transform will be applied before the class with
+    // its transition rule. 
+    const box = group.getBoundingClientRect();
+    group.classList.add('scrolling');
+
+    // If a user doesn't answer in time make the note fall below the staff
+    window.setTimeout(() => {
+        //const index = visibleNoteGroups.indexOf(group);
+        // if (index === -1) return;
+        group.classList.add('too-slow');
+        // visibleNoteGroups.shift();
+    }, 5000); }
+
+    counter+=1;
+
+    
+
+
+
+      }, 250);
 
         
 
@@ -407,7 +387,7 @@ afterSetStateFinished() {
       
   }
 
-  handleSubmit = (e) => {
+  handleGrading = (e) => {
     var note_dict = JSON.parse((String(this.state.grading).replace(/'/g,'"').replace(/\.0/g,".0").replace(/\.5/g,".50"))); 
     let correct_notes = []
     var incorrect_notes = []
@@ -415,8 +395,8 @@ afterSetStateFinished() {
     for (let note of this.state.recording.events) {
       
       let timestamp = (Math.round((note.time-5.236) * 4) / 4).toFixed(2);
-      
-      if(note_dict[timestamp] && note_dict[timestamp].indexOf(note.midiNumber)!=-1) {
+      if(timestamp<-4) {}
+      else if(note_dict[timestamp] && note_dict[timestamp].indexOf(note.midiNumber)!=-1) {
         
         var index = 0;
         note_dict[timestamp].splice(note_dict[timestamp].indexOf(note.midiNumber), 1)//note_dict[timestamp].remove(note.midiNumber);
@@ -438,10 +418,8 @@ afterSetStateFinished() {
     
     this.onClickClear();
     clearInterval(this.state.interval);
-    document.getElementById('boo').innerHTML ="";
+    document.getElementById('music').innerHTML ="";
     this.state.playing = false;
-    console.log(this.state.playing);
-    //return;
   
   };
 
@@ -488,7 +466,7 @@ afterSetStateFinished() {
         <div>
           <div id={'exercise-container'}>
           <div id="container">
-                <div id="boo"></div>
+                <div id="music"></div>
                 </div>
                 <div id="controls">
           </div>
@@ -500,7 +478,7 @@ afterSetStateFinished() {
           Unplayed: <div id = 'unplayed'></div>
              
         </div>
-        <button onClick={this.handleSubmit}>Grade</button>
+        <button onClick={this.handleGrading}>Grade</button>
           
       </div>
       
